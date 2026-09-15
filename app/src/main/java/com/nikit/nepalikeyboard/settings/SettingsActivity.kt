021f59622d0f4ba916d7819282b045ed8c719370
Nikit Hamal <iamnikithamal@gmail.com>
2026-09-15 21:53:14 +0545

    Fix patch omission: missing Surface import in UpdateBanner

--- app/src/main/java/com/nikit/nepalikeyboard/settings/SettingsActivity.kt
+++ app/src/main/java/com/nikit/nepalikeyboard/settings/SettingsActivity.kt
@@ -40,6 +40,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
